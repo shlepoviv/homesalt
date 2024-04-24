aa72804b530a978bf8f920b723853949828e0c22
Shlepov Ivan <Shlepov-Ivan@mail.ru>
2024-04-24 17:01:36 +0300

    change parse linux netadapter

--- _modules/audit.py
+++ _modules/audit.py
@@ -9,9 +9,6 @@ import logging
 import os
 import json
 import ipaddress
-import hashlib
-
-import salt.utils.cache as cache
 
 log = logging.getLogger(__name__)
 
@@ -25,6 +22,7 @@ def _linux_cdm(cmd:list[str],timeout:int = 3):
             outs, errs = proc.communicate(timeout=timeout)
         except subprocess.SubprocessError as e:
             proc.kill()
+            log.debug(f'audit _linux_cdm: {e}')
             log.debug(f'audit _linux_cdm: {e}')
             return 'None'
     return outs.decode('utf-8').rstrip()
@@ -112,71 +110,14 @@ def _linux_memdata(invent:dict):
     else: 
         log.debug(f'audit: file /sys/class/dmi/id/{file_meminfo} is not exist') 
 
-
 def _linux_host_disk2stor_map(invent:dict):
-    def convert_size(disk:dict):
-        if 'size' in disk:
-            disk['size'] = int(disk['size']) // 1024 // 1024
-        if 'children' in disk:
-            for c in disk['children']:
-                convert_size(c)
-    
-    def _replace_hex_escaped(s):
-        r = s.string[s.regs[0][0]+2:s.regs[0][1]]
-        r = chr(int((r),16))
-        return r
-    
-    def _parse_parm(line):
-        i = 0
-        start = 0
-        res = {}
-        for par in list_param:
-            while line[i] != ' ' and i < len(line)-1:
-                i += 1 
-            if start == i:
-                res[par] = None
-            else:         
-                res[par] = re.sub(r'\\x[0-9A-Fa-f][0-9A-Fa-f]',_replace_hex_escaped,line[start:i])
-            i += 1
-            start = i
-        return res
-
-    list_param = ['name', 'serial', 'vendor', 'model', 'size', 'type', 'mountpoint', 'pkname', 'kname']
-    out1 = _linux_cdm(['lsblk','-n','-io','name']).split('\n')
-    out2 = _linux_cdm(['lsblk','-n','-r','-ibo',','.join(list_param)]).split('\n')
-    
-    res = []
-    list_parent = []
-    prevlvl = 0
-    for line, line2 in zip(out1,out2):
-        i = 0       
-        while line[i] in (' ','`','|','-'):
-            i +=1
-        lvl = i//2
-        if lvl == 0:
-            res.append(_parse_parm(line2))
-            list_parent.clear()
-            prevlvl = 0
-        else:
-            if prevlvl < lvl:
-                if not list_parent:
-                    list_parent.append(res[-1])
-                else:
-                    list_parent.append(list_parent[-1]['children'][-1])
-                list_parent[-1]['children'] = [_parse_parm(line2)]
-                prevlvl = lvl
-            elif prevlvl == lvl:
-                list_parent[-1]['children'].append(_parse_parm(line2))             
-            elif prevlvl > lvl:
-                for _ in range(prevlvl - lvl):
-                    list_parent.pop()
-                list_parent[-1]['children'].append(_parse_parm(line2)) 
-                prevlvl = lvl
-
-    for disk in res:
-            convert_size(disk)  
-             
-    invent["host_disk2stor_map"] = res
+    colums = 'NAME,SERIAL,VENDOR,MODEL,SIZE,TYPE,MOUNTPOINT,PKNAME,KNAME'
+    out = _linux_cdm(['lsblk','-J','-b','-o',colums])
+    try:
+        invent["host_disk2stor_map"] = json.loads(out)['blockdevices']
+    except json.JSONDecodeError:
+        log.debug('audit: _linux_host_disk2stor_map JSONDecodeError, input{out}')
+        invent["host_disk2stor_map"] = 'None'
 
 
     
@@ -187,27 +128,50 @@ def _linux_hostnames(invent:dict):
     
 
 def _linux_netatapter(invent:dict):
-    out = _linux_cdm(['ip','-j','a'])
+    out = _linux_cdm(['ip','-j','-d','a'])
     list_adapt = []
-    for a in adaptdata:
-        adapt = {}
-        adapt['name'] = a['ifname']
-        adapt['mac_address'] = a['address']
-        adapt['ips_info'] = []
-        for addr in a['addr_info']:
-            i = ipaddress.ip_interface(f'{addr["local"]}/{addr["prefixlen"]}')
-            adapt['ips_info'].append({"ip": str(i.ip),
-                                     "netmask": str(i.netmask),
-                                     "network": str(i.network),
-                                     "alias": addr.get('label','')})
-        list_adapt.append(adapt)
+
+    adapt = None
+
+    for line in out:
+        m = pattern.search(line)
+        if m:
+            if adapt:
+                list_adapt.append(adapt)
+            adapt = {'name' : m.group(1),
+                    'ips_info' : []}
+        else:
+            if adapt:
+                sline = line.split()
+
+                if sline[0].startswith('link'):
+                    adapt['mac_address'] = sline[1]
+                
+                elif sline[0].startswith('inet'):
+
+                    if '/' in sline[1]:
+                        ip, pref = sline[1].split('/')
+                    else:
+                        ip = sline[1]
+                        pref = 32           
+
+                    i = ipaddress.ip_interface(f'{ip}/{pref}')
+                    
+                    adapt['ips_info'].append({"ip": str(i.ip),
+                                                "netmask": str(i.netmask),
+                                                "network": str(i.network),
+                                                "alias": sline[-1] if not sline[0].startswith('inet6') else None})
+        
+    if adapt:
+        list_adapt.append(adapt) 
+        
+    except json.JSONDecodeError:
+        log.debug('audit: _linux_host_disk2stor_map JSONDecodeError, input{out}')
     invent['host_netadapter_html'] = list_adapt
 
 def _linux_ip_address(invent:dict):
     invent['host_ip_address'] = _linux_cdm(['hostname','-i'])
-    out = _linux_cdm(['hostname','-I']).split()
-    out.append(invent['host_ip_address'])
-    invent['host_ip_addresses'] = out
+    invent['host_ip_addresses'] = _linux_cdm(['hostname','-I']).split().append(invent['host_ip_address'])
 
 def _linux_add_dns_servers(invent:dict):
     file_meminfo = '/run/systemd/resolve/resolv.conf'
@@ -226,13 +190,13 @@ def _linux_add_users(invent:dict):
     if os.path.exists(file_meminfo):
         with open(file_meminfo,'r',encoding='utf-8') as f: 
             for line in f:
-                u = line.strip().split(':')
+                u = line.split(':')
                 invent['host_users'].append({
                     "username": u[0],
-                    "uid": int(u[2]),
-                    "gid": int(u[3]),
-                    "home_dir": u[5],
-                    "shell": u[6]
+                    "uid": u[2],
+                    "gid": u[3],
+                    "home_dir": u[4],
+                    "shell": u[5]
 
                 })
     else: 
@@ -249,41 +213,7 @@ def _linux_add_pkgs_list(invent:dict):
         })  
 
 
-def _win_add_pass(invent:dict):
-    invent['host_pkgs_list'] = []
-    invent['host_users']=[]
-    invent['host_netadapter_html'] = []
-    invent["host_disk2stor_map"] = []
-
-def _write_to_cache(data):
-    cachedir = __salt__["config.get"]("cachedir")
-    context_cache = cache.ContextCache({'cachedir':cachedir}, "inventorycache")
-    context_cache.cache_context(data.copy())
-
-def _read_from_cache():
-    cachedir = __salt__["config.get"]("cachedir")
-    try:
-        context_cache = cache.ContextCache({'cachedir':cachedir}, "inventorycache")
-        return context_cache.get_cache_context()
-    except:
-        return None
-
-def _hash(dictionary:dict) -> str:
-    """MD5 hash of a dictionary."""
-    dhash = hashlib.md5()
-    encoded = json.dumps(dictionary, sort_keys=True).encode()
-    dhash.update(encoded)
-    return dhash.hexdigest()
-
-
-def _send_event(invent:dict):
-    __salt__["event.send"](
-            "custom/discovery/inventory/check",
-            {"finished": True, "message": "audit result",'inventory':invent},
-        )
-        
-
-def check(full=False, silent = False):
+def check():
     '''
     launching audite
     '''
@@ -305,32 +235,6 @@ def check(full=False, silent = False):
         _linux_add_dns_servers(invent)
         _linux_add_users(invent)
         _linux_add_pkgs_list(invent)
-    if invent['host_os_family_lu'].startswith('Windows'):
-        _win_add_pass(invent)
-    
-    old_cache = _read_from_cache() 
-    log.debug(f'old_cache : {old_cache}')
-    old_cache_hash = None
-    if old_cache:
-        old_cache_hash = _hash(old_cache)  
-    new_hash = _hash(invent)
-    _write_to_cache(invent)
-    invent['old_cache_hash'] = old_cache_hash
-    invent['new_hash'] = new_hash
-    if invent['new_hash'] != invent['old_cache_hash']:
-        log.debug('new cache != old cache')     
-    elif silent:
-        return invent
-
-    if not full:
-        for key, val in invent.items():
-            if isinstance(val,list):
-                if len(val) > 1:
-                    if isinstance(val[0],dict):
-                        invent[key] = f'count {len(val)}'
-
-    if invent:
-        _send_event(invent)
     return invent
 
 if __name__ == '__main__':
