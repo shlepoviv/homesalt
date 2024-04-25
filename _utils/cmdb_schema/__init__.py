b404ca737dcdad3f2ccbab93e561834e97c2cbbe
Shlepov Ivan <Shlepov-Ivan@mail.ru>
2024-04-25 17:03:36 +0300

    add merge in work with db

--- _utils/cmdb_schema/__init__.py
+++ _utils/cmdb_schema/__init__.py
@@ -15,7 +15,6 @@ _DB_PARAM = 'postgresql+psycopg2://postgres:d34gj8h9qNn@192.168.1.13/cmdb'
 class WorkWithDB():
     def __init__(self):
         self._get_engine()
-        self._init_table()
 
     def _get_engine(self):
         try:
@@ -24,7 +23,7 @@ class WorkWithDB():
         except Exception as e:
             log.error(f'util cmdb_schema: can not create engine - {e}')
     
-    def _init_table(self):
+    def init_table(self):
         Host.metadata.create_all(self.engine)
 
     def write_inventory(self,data):
@@ -33,18 +32,7 @@ class WorkWithDB():
         """
         if self.engine:       
             with Session(self.engine) as session:
-                host = session.get(Host,data['id'])
-                if host:
-                    log.debug(f'write_inventory: find {data["id"]} host in base')
-                    updatehost = Host(host_id=data['id'],**data['data']['inventory'])
-                    if host.get_diff(updatehost):
-                        log.debug(f'write_inventory: find {data["id"]} host in base with different attributes')
-                    else:
-                        log.debug(f'write_inventory: find {data["id"]} host in base')
-                else:
-                    host = Host(host_id=data['id'],**data['data']['inventory'])
-                session.add(host)
-                session.flush()
+                host = Host(host_id=data['id'],**data['data']['inventory'])
+                upgreded_host =  session.merge(host)
                 session.commit()
-                log.debug(f'write_inventory: {data["id"]} host create in base')
 
