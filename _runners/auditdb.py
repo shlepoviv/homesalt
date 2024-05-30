71a589b5dcd483d1594229540143ecacca4d49dd
Shlepov Ivan <Shlepov-Ivan@mail.ru>
2024-05-30 14:05:18 +0300

    eng

--- _runners/auditdb.py
+++ _runners/auditdb.py
@@ -7,12 +7,11 @@ from cmdb_schema import WorkWithDB
 from tracem import TraceM
 
 log = logging.getLogger(__name__)
-
+import salt.utils.json
 
 def write(data):
     log.debug('try write to db')
-    tr = TraceM()
-    db = WorkWithDB()
-    db.write_inventory(data)
-    tr.dumptrace()
+    jevent = salt.utils.json.dumps(data)
+    with open('/home/shlepov/log_test_engine','a') as f:
+        f.write(jevent+ "\n")
     return True
