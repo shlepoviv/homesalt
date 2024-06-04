cff86018e67c1465a633dec6e0cf163df7c806d7
Shlepov Ivan <Shlepov-Ivan@mail.ru>
2024-06-04 09:10:07 +0300

    bugfix

--- _runners/check_minions.py
+++ _runners/check_minions.py
@@ -1,5 +1,5 @@
 from custom_runners.minion_key_runner import MinionKey
 
-def check_dns_resolve(remove_key: bool = True):
+def check_dns_resolve(remove_key: bool = True,timeout:int = 0):
     min_key = MinionKey(__opts__['pki_dir'])
-    return min_key(remove_key)
+    return min_key.check_dns_resolv(remove_key,timeout)
