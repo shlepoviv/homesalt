511dd352551d7c085e43b8e31885c6ad73262657
Shlepov Ivan <Shlepov-Ivan@mail.ru>
2024-06-06 14:09:27 +0300

    f

--- sls/mem_control/mem_leak.py
+++ sls/mem_control/mem_leak.py
@@ -14,7 +14,7 @@ loging(f'start')
 
 for proc in psutil.process_iter():
     try:
-        
+        loging(f'proc {proc.name()} | {' '.join(proc.cmdline())}')
         if 'mem_leak.py' in ' '.join(proc.cmdline()):
             if proc.pid == current_process.pid:
                 continue
