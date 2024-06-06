7ce04abc0a69e4204edd835ad2cc397adb9f6708
Shlepov Ivan <Shlepov-Ivan@mail.ru>
2024-06-06 12:40:58 +0300

    f

--- sls/mem_control/mem_leak.py
+++ sls/mem_control/mem_leak.py
@@ -1,41 +1,36 @@
 # cat /opt/saltcmdb/modules/mem_leak.py
-import sys
-try:
+import psutil
+from datetime import datetime 
 
-    import psutil
-    from datetime import datetime 
+procs=[]
 
-    procs=[]
+current_process = psutil.Process()
 
-    current_process = psutil.Process()
+def loging(mess):
+    with open('/app/salt/var/log/salt/mem_leak.log','a')as f:
+        f.write(f'{datetime.now()} | {mess}\n')
 
-    def loging(mess):
-        with open('/app/salt/var/log/salt/mem_leak.log','a')as f:
-            f.write(f'{datetime.now()} | {mess}\n')
+for proc in psutil.process_iter():
+    try:
+        if 'mem_leak.py' in ' '.join(proc.cmdline()):
+            if proc.pid == current_process.pid:
+                continue
+            else:
+                psutil.sys.exit()
+        if '/bin/salt-minion' in ' '.join(proc.cmdline()):
+            if proc.pid != current_process.pid:
+                procs.append(proc)
+    except psutil.ZombieProcess:
+        pass
 
-    for proc in psutil.process_iter():
+loging(f'find {procs}')
+if procs:
+    alloc_mem = int(sum([i.memory_info().rss for i in procs])/1024/1024)
+    loging(f'procs mem: {alloc_mem}')
+    if alloc_mem > 135:
         try:
-            if 'mem_leak.py' in ' '.join(proc.cmdline()):
-                if proc.pid == current_process.pid:
-                    continue
-                else:
-                    psutil.sys.exit()
-            if '/bin/salt-minion' in ' '.join(proc.cmdline()):
-                if proc.pid != current_process.pid:
-                    procs.append(proc)
-        except psutil.ZombieProcess:
+            for proc in procs:
+                loging(f'kill proc {proc}')
+                proc.kill()
+        except:
             pass
-
-    loging(f'find {procs}')
-    if procs:
-        alloc_mem = int(sum([i.memory_info().rss for i in procs])/1024/1024)
-        loging(f'procs mem: {alloc_mem}')
-        if alloc_mem > 135:
-            try:
-                for proc in procs:
-                    loging(f'kill proc {proc}')
-                    proc.kill()
-            except:
-                pass
-except Exception:
-    sys.exit(1)
