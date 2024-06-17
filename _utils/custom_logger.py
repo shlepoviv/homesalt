e9c67a291b6a5697cea48644d3e3722f30e5ef02
Shlepov Ivan <Shlepov-Ivan@mail.ru>
2024-06-17 20:29:01 +0300

    f

--- _utils/custom_logger.py
+++ _utils/custom_logger.py
@@ -5,16 +5,19 @@ class CustomLogger:
         self.opts = opts
         self.last_check_opts = time.time()
         self.interval_check_opts = 120
+        self._check_opts_interval = 0
 
     def _check_opts(self):
         if time.time() - self.last_check_opts >= self.interval_check_opts:
+            start = time.time()
             self.opts = salt.config.master_config(self.opts['conf_file'])
+            self._check_opts_interval = time.time() - start
             self.last_check_opts = time.time()
 
     def info(self,mess):
         self._check_opts()
         with open('/home/shlepov/testlog','a') as f:
-            f.write(f'last_check_opts: {self.last_check_opts}\tlog_level: {self.opts.get("custom_log_lvl")}\t{mess}\n')
+            f.write(f'last_check_opts: {self.last_check_opts}\tlog_level: {self.opts.get("custom_log_lvl")}\t{self.last_check_opts}\n')
 
 
         
